5b8fd827e5713751b119677c46f76e363378665c
waridh <waridh@proton.me>
2024-08-31 18:33:03 -0400

    updated config to work well with polybar 3.5.7

--- launch.sh
+++ launch.sh
@@ -6,10 +6,13 @@
 # Otherwise you can use the nuclear option:
 killall -q polybar
 
-# Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
-polybar --config="$HOME/.config/polybar/config" bar1 2>&1 | tee -a /tmp/polybar1.log &
-# polybar bar2 2>&1 | tee -a /tmp/polybar2.log &
-# disown
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload --config="$HOME/.config/polybar/config" bar1 2>&1 | tee -a /tmp/polybar1.log &
+  done
+else
+  polybar --reload --config="$HOME/.config/polybar/config" bar1 2>&1 | tee -a /tmp/polybar1.log &
+fi
 
 echo "Bars launched..."
